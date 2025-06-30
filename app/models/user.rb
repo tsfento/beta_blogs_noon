d7bb3b68e2394bca7062d856bbda01ef42407cd9
Tyler Fenton <tsfento@gmail.com>
2025-06-30 14:40:33 -0500

    init commit

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_many :blogs
+  has_one :profile
+  after_create :create_profile
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :username, presence: true
+end
