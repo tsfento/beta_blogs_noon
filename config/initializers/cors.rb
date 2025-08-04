1d299634e4a4e085cb8623fa79db79525e465db6
Tyler Fenton <tsfento@gmail.com>
2025-08-04 12:52:18 -0500

    Added production urls to CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:4200"
+    origins "http://localhost:4200",
+    "https://fe-beta-blogs-noon.vercel.app",
+    "https://fe-beta-blogs-noon-tyler-fentons-projects.vercel.app/",
+    "https://fe-beta-blogs-noon-git-main-tyler-fentons-projects.vercel.app/"
 
     resource "*",
       headers: :any,
