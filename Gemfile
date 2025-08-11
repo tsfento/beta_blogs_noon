5e338f616ad497caff78490d6d9835bec092804b
Tyler Fenton <tsfento@gmail.com>
2025-08-11 12:23:15 -0500

    Added rate limiting with rack-attack

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem "rack-cors"
 gem "bcrypt"
 gem "jwt"
 gem "blueprinter"
+gem "rack-attack"
 
 group :production do
   gem "pg"
--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module BetaBlogsApiNoon
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use Rack::Attack
   end
 end
